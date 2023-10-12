11d8fa6752d35408d193243d1145e0ab43fb33d8
Eduardo <edkemssac@gmail.com>
2023-10-11 19:38:32 -0500

    definicion del primer componente

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import C01componente from './Componentes/C01componente';
 
 function App() {
   return (
     <div >
       <C01componente/>
+        
     </div>
   );
 }
