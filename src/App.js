07e20ffebe714b333269dad51c5bd65a1ca50dc1
Eduardo <edkemssac@gmail.com>
2023-10-17 19:42:40 -0500

    V3 instalacion y configuracion de boot, icons y toastify

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 import C01componente from './Componentes/C01componente';
 
 function App() {
   return (
     <div >
       <C01componente/>
-        
+      Hola Duda, estas en la clase del 17 oct 23
+      <i class="large material-icons">insert_chart</i>  
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//import 'bootswatch/dist/solar/bootstrap.min.css'; // agregando tema solar del bootswatch
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
