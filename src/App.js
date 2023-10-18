bef9c8c99c0966e14eae657cc252262fad401fdb
Eduardo <edC22@gmail.com>
2023-10-18 18:51:35 -0500

    V4 diseño de app.js y appform

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+//import './App.css';
+import C01componente from './Componentes/C01componente';
+import AppForm from './Componentes/AppForm';
+
+
+function App() {
+  return (
+    <div style={{width:"350px", background:"greenyellow", padding:"10px"}}>
+      <AppForm />
+      <p>No. 1 Juan manuel       x    A</p>
+      <p>No. 2 Juan manuel       x    A</p>
+    </div>
+  );
+}
+
+export default App;
