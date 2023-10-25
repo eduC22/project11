3f69f6ff4c4aceba155d9fd915565d73e2a924ee
Eduardo <edC22@gmail.com>
2023-10-24 19:39:17 -0500

    V7 Configuracion de autenticacion, firestore y storage

--- src/Componentes/AppForm.js
+++ src/Componentes/AppForm.js
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { db } from './firebase';
+import { addDoc, collection } from 'firebase/firestore';
 
-const AppForm = () => {
+
+
+const AppForm = () => { 
+  
+  //////// GUARDAR / ACTUALIZAR /////
+  const camposRegistro = {Nombre:"", Edad:"", Genero:""}
+  const[objeto, setobjeto] = useState (camposRegistro);
+  
+  const handleSubmit = (e) => {  ////manejador de submit
+      e.preventDefault();
+
+      try{
+         if(db){
+             addDoc(collection(db, 'cliente'), objeto)
+             console.log("Se guardo con exito")
+         }
+      } catch (error){
+          console.error();
+      }
+  }
   return (
     <div style={{background:"orange", padding:"10px", textAlign:"center"}}>
-      <h1>AppForm.js</h1>
-      <p>Nombre</p>
-      <p>Edad</p>
-      <p>Genero</p>
-      <p>Guardar</p>
+       <form onSubmit={handleSubmit}>
+        <h1>AppForm.js</h1>
+        <input type='text' placeholder='Nombre.....'/>
+        <input type='text' placeholder='Edad.....'/>
+        <input type='text' placeholder='Sexo.....'/>        
+        <select>
+          <option value="">Seleccione....</option>
+          <option value="M">Masculino</option>
+          <option value="F">Femenino</option>
+        </select><br></br>
+       <button>
+            GUARDAR/ACTUALIZAR
+       </button>
+       </form>  
+      
     </div>
   )
 }
