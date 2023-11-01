9c98108cf72c4322cf10a3911a862c97b1d213c8
Eduardo <edC22@gmail.com>
2023-10-31 19:32:18 -0500

    V11 SE CONFIGURO EL RUTEO A PAGINAS PUBLICAS

--- src/App.js
+++ src/App.js
@@ -1,61 +1,19 @@
-import logo from './logo.svg';
-//import './App.css';
-import C01componente from './Componentes/C01componente';
-import AppForm from './Componentes/AppForm';
-import { useEffect, useState } from 'react';
-import { collection, deleteDoc, doc, onSnapshot, query } from 'firebase/firestore';
-import { db } from './Componentes/firebase';
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Rutas from './ruteo/Rutas'
+import BarraNavegacion from './ruteo/BarraNavegacion'
 
-function App() {
-  ////////READ - LECTURA ////////
-  const [docBd, setDocBd] = useState([]);
-  const fnRead = () => {
-    try {
-      const xColeccionConQuery = query(collection(db, "persona"));
-
-      const unSubscrite = onSnapshot(xColeccionConQuery, (xDatosBD) => {
-        const xDoc = [];
-        xDatosBD.forEach( (doc) => {
-         xDoc.push({id: doc.id, ...doc.data()});
-        });
-        setDocBd(xDoc);
-      });
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-//fnRead();
-useEffect( () => {
-  fnRead();
-}, []);
-
-/////// DELETE - ELIMINAR /////////
-  const [idActual,setIdActual] = useState("");
-  const fnDelete = async (xId) => {
-    if(window.confirm("Confirme para eliminar")){
-      await deleteDoc(doc(db, 'persona', xId));
-      console.log("Se elimino...."+xId)
-    }
-
-  }
-
+const App = () => {
   return (
-    <div style={{width:"350px", background:"greenyellow", padding:"10px"}}>
-      <AppForm {...{idActual, setIdActual, fnRead}}/>
-      {
-        docBd.map((p) =>
-          <p key={p.id}>
-            No. {p.nombre}...
-            <span onClick={() => fnDelete(p.id)}>x</span>
-            .....
-            <span onClick={() => setIdActual(p.id)}>A</span>
-          </p>
-        )
-      }  
-
+    <div style={{background:"violet"}}>
+      <h1>App.js</h1>
+      <Router>
+        <BarraNavegacion />
+        <Rutas />       
+      </Router>
+      Pagina de app.js
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
